8e0ad98f2c9258957727be8073f9a2faa1b5989e
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-11-15 15:31:48 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Todo-List Uygulaması
+
+### Bu proje gerçek anlamda birçok şey kattı [patikadev](https://app.patika.dev/) ailesine teşekkür ederim , bu güzel program için
+
+### Bu uygulamada öğrendiğim ve pekiştirdiğim
+
+- Components
+- State
+- UseEffect
+- UseState
---
+++ src/components/footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer className="info">
+      <p>
+        Created by{" "}
+        <a href="https://www.linkedin.com/in/ayhan-d-03a76220a/">
+          Ayhan Daşyuvarlar
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
